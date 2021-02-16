8f5df2a33a633b5d16a73e2f611a9c827ac9dd8c
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2021-02-16 15:05:58 -0300

    Adicionando properties

--- GestaoProduto/Produto.cs
+++ GestaoProduto/Produto.cs
@@ -7,43 +7,65 @@ namespace GestaoProduto
 {
     class Produto
     {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         public Produto(string nome, double preco, int quantidade)
         {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
         }
 
         public Produto(string nome, double preco)
         {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = 0;
+            _nome = nome;
+            _preco = preco;
+            _quantidade = 0;
         }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1) ;
+            }
+        }
+
+        public double Preco
+        {
+            get { return _preco; }
+           
+        }
+
+        public int Quantidade
+        {
+            get { return _quantidade; }
+
+        }
+
 
         public double ValorTotalEmEstoque()
         {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProduto(int qte)
         {
-            Quantidade = Quantidade + qte;
+            _quantidade = _quantidade + qte;
         }
 
         public void RemoverProduto(int qte)
         {
-            Quantidade = Quantidade - qte;
+            _quantidade = _quantidade - qte;
         }
 
         public override string ToString()
         {
-            return $"{Nome}, R${Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
-                $"{Quantidade} unidades, Total: R$ R${ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+            return $"{Nome}, R${_preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
+                $"{_quantidade} unidades, Total: R$ R${ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
         }
     }
 }
