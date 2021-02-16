185f4294e717c293503f9b25dd7f0e55065f2c1c
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2021-02-16 12:49:18 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoProduto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GestaoProduto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
