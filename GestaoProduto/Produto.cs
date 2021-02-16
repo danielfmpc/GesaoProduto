9d824d4f491405eff63f00611663f97184e57e25
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2021-02-16 13:09:10 -0300

    Override ToString + Metodo

---
+++ GestaoProduto/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestaoProduto
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, R${Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
+                $"{Quantidade} unidades, Total: R$ R${Quantidade.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
